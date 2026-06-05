e293fa3848f17067cc7fcc9384ffa752174fd814
helpofai <contact@helpofai.com>
2026-06-05 19:54:12 +0530

    refactor: update plugin core file

--- hoa-movie-mart-core.php
+++ hoa-movie-mart-core.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: HOA Movie Mart Core
  * Description: Core functionality for HOA Movie Mart theme, including Movie Custom Post Type and Taxonomies.
- * Version: 2.0.3
+ * Version: 2.0.5
  * Author: the HelpOfAi team
  * Text Domain: helpofai
  */
